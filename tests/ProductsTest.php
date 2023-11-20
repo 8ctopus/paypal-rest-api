9be35d0c80f3470a960495c7e40a549d0563900d
8ctopus <hello@octopuslabs.io>
2023-11-20 11:27:30 +0400

    Add products test

---
+++ tests/ProductsTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Nimbly\Capsule\Factory\RequestFactory;
+use Nimbly\Capsule\Factory\StreamFactory;
+use Nimbly\Capsule\Response;
+use Nimbly\Shuttle\Shuttle;
+use Oct8pus\PayPal\Products;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ *
+ * @covers \Oct8pus\PayPal\Products
+ */
+final class ProductsTest extends TestCase
+{
+    private static HttpHandlerMock $handler;
+    private static OAuthMock $auth;
+    private static Products $plans;
+
+    public static function setUpBeforeClass() : void
+    {
+        self::$handler = new HttpHandlerMock(new Shuttle(), new RequestFactory(), new StreamFactory());
+        self::$auth = new OAuthMock(self::$handler, 'testId', 'testSecret');
+        self::$plans = new Products(self::$handler, self::$auth);
+    }
+
+    public function testList() : void
+    {
+        self::$handler->setResponse(new Response(200, file_get_contents(__DIR__ . '/fixtures/ProductsList.json')));
+
+        self::$plans->list();
+
+        $expected = <<<TEXT
+        https://api-m.sandbox.paypal.com/v1/catalogs/products
+        Host: api-m.sandbox.paypal.com
+        Authorization: Bearer test
+        Content-Type: application/json
+
+        TEXT;
+
+        self::assertSame($expected, self::$handler->dumpRequest());
+    }
+
+    public function testGet() : void
+    {
+        self::$handler->setResponse(new Response(200, file_get_contents(__DIR__ . '/fixtures/ProductDetails.json')));
+
+        $id = 'P-5ML4271244454362WXNWU5NQ';
+
+        self::$plans->get($id);
+
+        $expected = <<<TEXT
+        https://api-m.sandbox.paypal.com/v1/catalogs/products/{$id}
+        Host: api-m.sandbox.paypal.com
+        Authorization: Bearer test
+        Content-Type: application/json
+
+        TEXT;
+
+        self::assertSame($expected, self::$handler->dumpRequest());
+    }
+
+    public function testAdd() : void
+    {
+    }
+}
