d8dea241562599c8abee449952c35c51b7a052ba
8ctopus <hello@octopuslabs.io>
2023-11-20 11:21:03 +0400

    Improve plans tests

--- tests/PlansTest.php
+++ tests/PlansTest.php
@@ -45,4 +45,61 @@ final class PlansTest extends TestCase
 
         self::assertSame($expected, self::$handler->dumpRequest());
     }
+
+    public function testGetPlan() : void
+    {
+        self::$handler->setResponse(new Response(200, file_get_contents(__DIR__ . '/fixtures/PlanDetails.json')));
+
+        $id = 'P-5ML4271244454362WXNWU5NQ';
+
+        self::$plans->get($id);
+
+        $expected = <<<TEXT
+        https://api-m.sandbox.paypal.com/v1/billing/plans/{$id}
+        Host: api-m.sandbox.paypal.com
+        Authorization: Bearer test
+        Content-Type: application/json
+
+        TEXT;
+
+        self::assertSame($expected, self::$handler->dumpRequest());
+    }
+
+    public function testActivate() : void
+    {
+        self::$handler->setResponse(new Response(204, ''));
+
+        $id = 'P-5ML4271244454362WXNWU5NQ';
+
+        self::$plans->activate($id);
+
+        $expected = <<<TEXT
+        https://api-m.sandbox.paypal.com/v1/billing/plans/{$id}/activate
+        Host: api-m.sandbox.paypal.com
+        Authorization: Bearer test
+        Content-Type: application/json
+
+        TEXT;
+
+        self::assertSame($expected, self::$handler->dumpRequest());
+    }
+
+    public function testDeactivate() : void
+    {
+        self::$handler->setResponse(new Response(204, ''));
+
+        $id = 'P-5ML4271244454362WXNWU5NQ';
+
+        self::$plans->deactivate($id);
+
+        $expected = <<<TEXT
+        https://api-m.sandbox.paypal.com/v1/billing/plans/{$id}/deactivate
+        Host: api-m.sandbox.paypal.com
+        Authorization: Bearer test
+        Content-Type: application/json
+
+        TEXT;
+
+        self::assertSame($expected, self::$handler->dumpRequest());
+    }
 }
